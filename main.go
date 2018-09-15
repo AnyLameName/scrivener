b49fdbd7aae6e30be9275bf6aa2349f93a69912f
Jay Gagnon <jgagnon@pc.factset.com>
2018-09-14 22:04:56 -0400

    Bad code, no error-handling, but if the fuzzy match succeeds, I can find the card's name. So that's nice.

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
+    "io/ioutil"
     "log"
     "net/http"
     "os"
@@ -10,10 +12,28 @@ import (
     _ "github.com/heroku/x/hmetrics/onload"
 )
 
+type Card struct {
+    Name string `json:"name"`
+}
+
 func cardCallback(c *gin.Context) {
+    urlBase := "https://api.scryfall.com/cards/named?fuzzy=%s"
     cardName := c.Param("name");
-    response := fmt.Sprintf("Requested: '%s'", cardName)
-    c.String(http.StatusOK, response);
+    url := fmt.Sprintf(urlBase, cardName)
+    resp, err := http.Get(url)
+    if(err != nil) {
+        // Sure, sure. Error handling. Of course.
+    }
+
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+
+    var card Card
+    err = json.Unmarshal(body, &card)
+
+    log.Printf("Card name: %s", card.Name)
+
+    c.String(http.StatusOK, fmt.Sprintf("Card Name: %s", card.Name));
 }
 
 func main() {
